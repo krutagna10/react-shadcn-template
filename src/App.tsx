740de5519edfc7a7e64af3c8b316ee1217de8eb0
Krutagna Patel <kpatel.kp3101@gmail.com>
2025-02-28 16:54:41 +0530

    add theme-toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <main>
         <h1>React ShadCN Template</h1>
+        <ModeToggle />
       </main>
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
